ebf2abc4871f53d84fec1d800eeee825114e6867
cloudzeno <87767391+cloudzeno@users.noreply.github.com>
2024-07-23 09:37:33 -0400

    Update leaderstats.lua

--- leaderstats.lua
+++ leaderstats.lua
@@ -1,1 +1,25 @@
+local Players = game:GetService("Players")
 
+local function leaderboardSetup(player)
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+
+	local kills = Instance.new("IntValue")
+	kills.Name = "Kills"
+	kills.Value = 0
+	kills.Parent = leaderstats
+	
+	local hits = Instance.new("IntValue")
+	hits.Name = "Hits"
+	hits.Value = 0
+	hits.Parent = leaderstats
+	
+	local deaths = Instance.new("IntValue")
+	deaths.Name = "Deaths"
+	deaths.Value = 0
+	deaths.Parent = leaderstats
+end
+
+
+Players.PlayerAdded:Connect(leaderboardSetup)
